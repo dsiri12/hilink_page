ee0858b1642e6626ed55bfd125b94526c94911bf
Damien Siri <dsiri12@gmail.com>
2023-10-24 13:08:53 -0400

    new updated changes

--- components/Button.tsx
+++ components/Button.tsx
@@ -4,13 +4,16 @@ type ButtonProps = {
   type: 'button' | 'submit'
   title: string
   icon?: string
-  variant: 'btn_dark_green'
+  variant: string
+  full?: boolean
 }
 
-const Button = ({ type, title, icon, variant }: ButtonProps) => {
+const Button = ({ type, title, icon, variant, full }: ButtonProps) => {
   return (
     <button
-      className={`flexCenter gap-3 rounded-full border ${variant}`}
+      className={`flexCenter gap-3 rounded-full border ${variant} ${
+        full && 'w-full'
+      }`}
       type={type}
     >
       {icon && (
@@ -21,7 +24,9 @@ const Button = ({ type, title, icon, variant }: ButtonProps) => {
           height={24}
         />
       )}
-      <label className='bold-16 whitespace-nowrap'>{title}</label>
+      <label className='bold-16 whitespace-nowrap cursor-pointer'>
+        {title}
+      </label>
     </button>
   )
 }
